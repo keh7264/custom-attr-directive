9a8fe5a1536ca33d2b4be9069c58977da335d636
ehkim <keh7264@gmail.com>
2018-06-20 00:03:17 +0900

    attribute directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,19 @@ import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-      <p textColor [color]="color">Text Color</p>
-      <button myDirective [inputValue]="msg" staticValue="hi">Click me</button>
+  <h1>Attribute Directive</h1>
+
+  <h4>텍스트의 컬러를 선택하세요</h4>
+  <div>
+    <input type="radio" name="colors" (click)="color='red'">Red
+    <input type="radio" name="colors" (click)="color='blue'">Blue
+    <input type="radio" name="colors" (click)="color='green'">Green
+  </div>
+
+  <p [textColor]="color" defaultColor="violet">Text Color</p>
   `,
   styles: []
 })
 export class AppComponent {
-  color = 'red';
-  msg = 'button click';
+  color: string;
 }
--- src/app/text-color.directive.ts
+++ src/app/text-color.directive.ts
@@ -1,22 +1,26 @@
-import { Directive, Input, ElementRef, Renderer2, HostListener } from '@angular/core';
+import { Directive, Input, ElementRef, Renderer2, HostListener, OnInit } from '@angular/core';
 
 @Directive({
   // tslint:disable-next-line:directive-selector
   selector: '[textColor]'
 })
-export class TextColorDirective {
+export class TextColorDirective implements OnInit {
+  // tslint:disable-next-line:no-input-rename
+  @Input('textColor') color: string;
+  @Input() defaultColor: string;
 
-  @Input() color: string;
+  ngOnInit(): void {
+    this.textColor(this.defaultColor);
+  }
 
   constructor(private el: ElementRef, private renderer: Renderer2) { }
-
 
   @HostListener('mouseenter') handleMouseEnter() {
     this.textColor(this.color);
   }
 
   @HostListener('mouseleave') handleMouseLeave() {
-    this.textColor(null);
+    this.textColor(this.defaultColor);
   }
 
   private textColor(color: string) {
